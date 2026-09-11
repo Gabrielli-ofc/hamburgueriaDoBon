ef80f8a234490d54893ea0e75cefc6db02184eef
Gabrielli Frivoli <frivoligabrielli@gmail.com>
2026-09-10 22:08:32 -0300

    Primeiro processo de tratamento de dados

---
+++ pandasFuncionando.py
@@ -0,0 +1,8 @@
+#Arquivo criado para testar se o pandas está funcionando, ao executar no terminal "python pandasFuncionando.py" e pressionar ENTER e a mensagem aparecer "pandas funcionando!" então o pandas está funcionando perfeitamente
+
+
+import pandas as pd
+print("pandas funcionando!")
+
+
+
---
+++ tratamentoDadosBonsBurguers.py
@@ -0,0 +1,9 @@
+
+import pandas as pd #comando para importar o pandas.
+
+df = pd.read_csv("bons_Burguers.csv") #comando para ler o arquivo csv e armazená-lo dentro da variável df (dataFrame).
+
+print(df) #comando para imprimir e mostrar o conteúdo do arquivo(dataFrame) no terminal.
+
+print(df.dtypes) #mostra o tipo de cada coluna da dataFrame.
+
